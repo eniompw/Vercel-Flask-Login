dfab781d78329d8e7135ce8a5d6b2bf2b142fd2e
eniompw <71254710+eniompw@users.noreply.github.com>
2023-09-06 03:20:15 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,5 +9,13 @@ def home():
 	
 @app.route('/debug')
 def debug():
-	url = os.environ["POSTGRES_URL"]
-	return url
+	conn = psycopg2.connect(
+    	host=os.environ["POSTGRES_HOST"],
+    	database=os.environ["POSTGRES_DATABASE"],
+    	user=os.environ["POSTGRES_USER"],
+    	password=os.environ["POSTGRES_PASSWORD"])
+	
+	cur = con.cursor()
+	cur.execute("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
+	res = cur.fetchall()
+	return str(res)
