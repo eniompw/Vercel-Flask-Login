6a44e9cf7bbefc6371e63c72c63b0502c95114e0
eniompw <71254710+eniompw@users.noreply.github.com>
2023-09-06 10:31:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import os
 
 app = Flask(__name__)
 
-conn_dict =  psycopg2.conninfo.conninfo_to_dict(os.environ["POSTGRES_URL"])
-
-#con = psycopg2.connect(
-#host=os.environ["POSTGRES_HOST"],
-#database=os.environ["POSTGRES_DATABASE"],
-#user=os.environ["POSTGRES_USER"],
-#password=os.environ["POSTGRES_PASSWORD"])
+con = psycopg2.connect(
+host=os.environ["POSTGRES_HOST"],
+database=os.environ["POSTGRES_DATABASE"],
+user=os.environ["POSTGRES_USER"],
+password=os.environ["POSTGRES_PASSWORD"])
 
 @app.route('/')
 def home():
@@ -18,7 +16,6 @@ def home():
 
 @app.route('/create')
 def create():
-	con = psycopg2.connect(**conn_dict)
 	cur = con.cursor()
 	cur.execute(	"""	CREATE TABLE users(
 				Username VARCHAR(20) NOT NULL PRIMARY KEY,
@@ -28,13 +25,6 @@ def create():
 	con.commit()
 	return 'CREATE'
 
-@app.route('/tables')
-def tables():
-	cur = con.cursor()
-	cur.execute("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
-	rows = cur.fetchall()
-	return str(rows)
-
 @app.route('/insert')
 def insert():
 	cur = con.cursor()
@@ -42,6 +32,7 @@ def insert():
 					VALUES ("Bob", "123");
 			""")
 	con.commit()
+	con.close()
 	return 'INSERT'
 
 @app.route('/select')
