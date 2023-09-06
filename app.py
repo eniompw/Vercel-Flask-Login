b67918d8f6e23c88623492743744d24a702d20d3
eniompw <71254710+eniompw@users.noreply.github.com>
2023-09-06 10:42:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from psycopg2.extensions import parse_dsn
 import os
 
 app = Flask(__name__)
+con = psycopg2.connect(**parse_dsn(os.environ["POSTGRES_URL"]))
 
 @app.route('/')
 def home():
@@ -14,12 +15,10 @@ def insert():
 	cur = con.cursor()
 	cur.execute("INSERT INTO users VALUES ('Bob', '123')")
 	con.commit()
-	con.close()
 	return 'INSERT'
 
 @app.route('/select')
 def select():
-	con = psycopg2.connect(**parse_dsn(os.environ["POSTGRES_URL"]))
 	cur = con.cursor()
 	cur.execute("SELECT * FROM users")
 	rows = cur.fetchall()
