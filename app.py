5e05b0a2b370ad7c324a4c0a7949bb05e7140607
eniompw <71254710+eniompw@users.noreply.github.com>
2023-09-06 10:26:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-conn_dict =  psycopg2.conninfo.conninfo_to_dict(os.environ["POSTGRES_URL"])
+#conn_dict =  psycopg2.conninfo.conninfo_to_dict(os.environ["POSTGRES_URL"])
 
 #con = psycopg2.connect(
 #host=os.environ["POSTGRES_HOST"],
