9fb8635876375477871616ac43562ba7dc8063b9
eniompw <71254710+eniompw@users.noreply.github.com>
2023-09-06 03:00:39 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('login.html')
+	
+@app.route('/debug')
+def debug():
+	return str(os.environ)
